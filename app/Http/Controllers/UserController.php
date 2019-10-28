2ac9a57dc6d79baea50ef7796135af53edb1ec9b
unknown <erwin@gostyle.studio>
2019-10-28 14:10:55 +0800

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Meeting;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $Credentials = ['email' => $request->email, 'password' => $request->password, 'status' => 'active'];
+        $authenticate = Auth::attempt($Credentials);
+        if ($authenticate) {
+            $accesstoken = request('accesstoken');
+            $user = Auth::user();
+            $success['token'] = $user->createToken($accesstoken)->accessToken;
+
+            return response()->json($success ? $success : "Internal Server Error", $user ? 200 : 500);
+
+        } else {
+            return response()->json(["error" => 'Email or Password is Invalid / Account not Activated'], 412);
+        }
+    }
+
+    // /**
+    //  * Show the form for creating a new resource.
+    //  *
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function create()
+    // {
+    //     //
+    // }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:50',
+            'brand_company' => 'required|string|max:50',
+            'phone_number' => 'digits:11|numeric',
+            'email' => 'required|email|unique:users,email',
+            'package' => 'required|string',
+            'password' => 'required|min:8|confirmed',
+            'status' => 'required|string',
+            'location' => 'required|string',
+            'place' => 'required',
+            'date' => 'required|date',
+            'time' => 'required'
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 412);
+        }
+        $input = $request->all();
+        $input['password'] = bcrypt($request->password);
+        $user = User::create($input);
+
+        $input['user_id'] = $user->id;
+        $meeting = Meeting::create($input);
+
+        return response()->json($user || $meeting ? ['user' => $user, 'meeting' => $meeting] : "Internal Server Error", $user || $meeting ? 200 : 500);
+    }
+
+    // /**
+    //  * Display the specified resource.
+    //  *
+    //  * @param  int  $id
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function show($id)
+    // {
+    //     //
+    // }
+
+    // /**
+    //  * Show the form for editing the specified resource.
+    //  *
+    //  * @param  int  $id
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function edit($id)
+    // {
+    //     //
+    // }
+
+    // /**
+    //  * Update the specified resource in storage.
+    //  *
+    //  * @param  \Illuminate\Http\Request  $request
+    //  * @param  int  $id
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function update(Request $request, $id)
+    // {
+    //     //
+    // }
+
+    // /**
+    //  * Remove the specified resource from storage.
+    //  *
+    //  * @param  int  $id
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function destroy($id)
+    // {
+    //     //
+    // }
+}
---
+++ database/migrations/2019_10_28_053947_create_meeting_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMeetingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meetings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');  
+            $table->string('place');
+            $table->date('date');
+            $table->time('time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meeting');
+    }
+}
